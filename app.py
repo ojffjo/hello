657d848b1aab66c758778290fea73fd538d3b452
JIAB <jiab@Nattcha.local>
2020-08-17 00:06:14 -0500

    More Sample

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
-course = "Python for Beginer"
-print(course[0:5])
+# Python Tutorial with "Mosh Hamedani"
 
-name = input("What is your name? ")
-weight_lbs = input('What is your weight (lbs)? ')
-weight_kg = float(weight_lbs) * 0.45
-weight = str(weight_kg)
-print(name + ", you weigh " + weight + " Kg.")
-height_m = input('How tall are you (m)? ')
-height = float(height_m)
-bmi = weight_kg / (height ** 2)
-print(f"Your BMI is {bmi}")
-print("Your BMI is " "%.2f" % bmi)
+# course = "Python for Beginners"
+# print(course[0:5])
+# print(len(course))  # to count number of character include space
+# print(course.upper())
+# print(course.lower())
+# print(course.title())
+# print(course.find("Beginners"))  # Return the index of "B"
+# print('Python' in course)  # Return Boolean values, True or False
+# print(course.replace("Beginners", "Absolute Beginners"))
+#
+# a = "This is the island of istanbul"
+# # 3 is the maximum replacement that can be done in the string
+# print(a.replace("is", "was", 3))  # Output --> Thwas was the wasland of istanbul
+
+
+# If clause
+# Score range 300-850, Bad credit is below 670
+credit_score = input('What is you "Credit Score"? ')
+print()
+credit_score = int(credit_score)
+price = 1000000
+if credit_score in range(670, 850):
+    down_payment = 0.1
+    down_payment *= price
+    print(f'You have Good Credit, so your down payment is only ${down_payment:.2f}')
+elif credit_score in range(300, 669):
+    down_payment = 0.2
+    down_payment *= price
+    print(f'You have Bad Credit, so you have to put down ${down_payment:.2f}')
+else:
+    print('Error! "Invalid Credit Score" \nPlease, type in your Credit Score between 300 - 850')
--- ifClause.py
+++ ifClause.py
@@ -1,28 +1,56 @@
 # if clause
-name = "Jiab"
-weight_kg = 50
-height_m = 1.62
+# name = "Jiab"
+# weight_kg = 50
+# height_m = 1.62
+#
+# bmi = weight_kg / (height_m ** 2)
+# print(bmi)
+#
+# if bmi < 25:
+#     print("Hi, Jiab, you are in a good shape.")
+# elif bmi == 25:
+#     print("Hi, Jiab, you are almost there.")
+# else:
+#     print("Hi, Jiab, you are overweight should diet quickly.")
+#
+# # Function
+# def bmiCal(w, h, n):
+#     calBmi = (0.45 * w) / (h ** 2)
+#     print(calBmi)
+#     if calBmi < 25:
+#         print(f"Hi, {n}, you are in a good shape.")
+#     else:
+#         print(f"Hi, {n}, you are overweight should diet quickly.")
+#
+#
+# bmiCal(160, 1.63, 'Tim')
+# bmiCal(112, 1.62, 'J')
+# print("This is out of bmiCal function.")
 
-bmi = weight_kg / (height_m ** 2)
+name = input("What is your name? ")
+weight_lbs = input('What is your weight (lbs)? ')
+weight_kg = float(weight_lbs) * 0.45
+weight = str(weight_kg)
+print(name + ", you weigh " + weight + " Kg.")  # Didn't use formatted string
+height_m = input('How tall are you (m)? ')
+height = float(height_m)
+bmi = weight_kg / (height ** 2)
 print(bmi)
+print()
+print(f"Your BMI is {bmi:.2f}")  # New formatted string
+print('\n')
+print("Your BMI is %.2f" % bmi)  # Old formatted string
 
-if bmi < 25:
-    print("Hi, Jiab, you are in a good shape.")
-elif bmi == 25:
-    print("Hi, Jiab, you are almost there.")
+if bmi < 18.5:
+    print(f'Hi, {name}, you are Underweight.')
+
+elif 18.5 <= bmi <= 24.9:
+    print('')
+    print(f'Hi, {name}, you are in a good shape.')
+
+elif 25 <= bmi <= 29.9:
+    print(f'Hi, {name}, you are Overweight.')
+
 else:
-    print("Hi, Jiab, you are overweight should diet quickly.")
+    print(f'Hi, {name}, you are Obese.')
 
-# Function
-def bmiCal(w, h, n):
-    calBmi = (0.45 * w) / (h ** 2)
-    print(calBmi)
-    if calBmi < 25:
-        print(f"Hi, {n}, you are in a good shape.")
-    else:
-        print(f"Hi, {n}, you are overweight should diet quickly.")
-
-
-bmiCal(160, 1.63, 'Tim')
-bmiCal(112, 1.62, 'J')
-print("This is out of bmiCal function.")
