e3c9215de3c7817481da79782840e8d9ab56d247
JIAB <ojffjo@gmail.com>
2020-08-17 00:28:58 -0500

    test user

--- ifClause.py
+++ ifClause.py
@@ -37,6 +37,7 @@ height = float(height_m)
 bmi = weight_kg / (height ** 2)
 print(bmi)
 print()
+print('\n')
 print(f"Your BMI is {bmi:.2f}")  # New formatted string
 print("Your BMI is %.2f" % bmi)  # Old formatted string
 
