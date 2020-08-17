d405e018b2bb7e775f9c862dc3fb2a087c692ec6
JIAB <ojffjo@gmail.com>
2020-08-17 13:13:43 -0500

    test commiter

--- ifClause.py
+++ ifClause.py
@@ -38,6 +38,7 @@ bmi = weight_kg / (height ** 2)
 print(bmi)
 print()
 print('\n')
+print('\n')
 print(f"Your BMI is {bmi:.2f}")  # New formatted string
 print("Your BMI is %.2f" % bmi)  # Old formatted string
 
